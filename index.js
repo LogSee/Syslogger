482b94c2e6089a9cabb6ea7ade98d7117d0e3148
Ainsey11 <ainsey11@users.noreply.github.com>
2019-04-29 20:12:10 +0100

    updating

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 var Syslogd = require('syslogd');
-const parser = require("nsyslog-parser");
+const mongo = require('mongodb').MongoClient
 
+
+var SYSLOG_PORT = process.env.SYSLOG_PORT || 514
+var MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017'
 
 Syslogd(function(info) {
+   
    console.log(info);
-   // var logline = info.msg
-   // console.log(parser(logline));
+
 })
-.listen(514, function(err) {
-    console.log('start')
+.listen(SYSLOG_PORT, function(err) {
+    console.log('Syslog Server Listening on port:', SYSLOG_PORT)
     if (err){
         console.error(err);
     }
